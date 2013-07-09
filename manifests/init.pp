d0d91bf704aacb879122f2fc78eb0c7da9e100f7
hirocaster <hohtsuka@gmail.com>
2013-07-09 18:23:47 +0900

    Add manifest install nottingham

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,6 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class nottingham {
+  package { 'Nottingham':
+    provider => 'compressed_app',
+    source   => 'http://dl.clickontyler.com/nottingham/nottingham20_2.1.3.zip',
+  }
 }
